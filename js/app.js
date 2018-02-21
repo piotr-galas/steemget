ce0d6c6dfbf5fb043ba6b8ce37a7c3e7c1daf45e
Piotr <galaspiotrek@gmail.com>
2018-02-21 19:42:36 +0100

    fix css path

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ class Steemget{
   includeAssets(){
     this.includeJs('https://cdn.steemjs.com/lib/latest/steem.min.js');
     this.includeJs('https://cdn.rawgit.com/showdownjs/showdown/1.8.5/dist/showdown.min.js');
-    this.includeCss(`${document.currentScript.src}/css/app.css`);
+    this.includeCss('https://cdn.jsdelivr.net/npm/steemget/css/app.css');
     this.includeCss('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600');
     this.includeCss('https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600')
   }
